8ad6e48450af3658f10fa0724deea08a6c2b25b1
Jennifer Gomez <jenniferaida@hotmail.com>
2026-06-25 16:44:18 -0500

    CORS fijo para frontend

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const app = express();
 app.use((req, res, next) => {
   const origin = req.headers.origin;
 
-  if (origin === "https://asegurate-frontend.vercel.app") {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
+  res.setHeader("Access-Control-Allow-Origin", "https://asegurate-frontend.vercel.app");
   res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
