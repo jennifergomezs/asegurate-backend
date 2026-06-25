ffc8ab7ece151ece3b4f901e72a5de3f16e6144b
Jennifer Gomez <jenniferaida@hotmail.com>
2026-06-25 17:51:15 -0500

    Corrige MONGODB

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-
+console.log("ENV MONGODB_URI:", process.env.MONGODB_URI ? "SI EXISTE" : "NO EXISTE");
 // Logs extra opcionales
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
